8a31b872bb6f2bfaf2fe4e55aad0e1f4953cc357
nishant <nishant.singh87@gmail.com>
2019-12-15 22:38:44 +0530

    add counters.

--- src/app/shared/productCard/index.js
+++ src/app/shared/productCard/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
+const Inrementor = ({ value, onIncrease, onDecrease }) => (
+  <div className="product-card__changequantity">
+    <span className="increment fas fa-plus" onClick={onIncrease} />
+    {value}
+    <span className="decrement fas fa-minus" onClick={onDecrease} />
+  </div>
+);
+
 export default ({
   imgSrc,
   title,
@@ -14,9 +22,13 @@ export default ({
       <div className="product-card__title">{title}</div>
       <div className="product-card__subtitle">{subTitle}</div>
       <div className="product-card__boldtext">{boldText}</div>
-      <div className="product-card__additem"> ADD </div>
-      <div className="product-card__changequantity">{quantityAdded}</div>
-      <div className="product-card__badgetext">{badgeText}</div>
+
+      {quantityAdded ? (
+        <Inrementor value={quantityAdded} />
+      ) : (
+        <div className="product-card__additem"> ADD </div>
+      )}
+      {badgeText && <div className="product-card__badgetext">{badgeText}</div>}
     </div>
   );
 };
